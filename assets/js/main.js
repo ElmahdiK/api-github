29c5818176009a0611facb7e09ad522c0b0b0a5b
ElmahdiK <elmahdi.korfed@gmail.com>
2024-10-08 06:22:51 +0200

    fix(index): gestion du responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,27 @@
 // https://api.github.com/users?per_page=8
-
-// https://www.azur-web.com/astuces/javascript-requette-http-ajax
-
 // https://docs.github.com/en/rest/users/users?apiVersion=2022-11-28#get-a-user
-
+// https://www.azur-web.com/astuces/javascript-requette-http-ajax
 
 window.onload = _ => {
     console.log("page loaded");
+    loadDogs();
+}
 
-
+const loadDogs = _ => {
     const headers = new Headers({
         "Content-Type": "application/json",
         "x-api-key": "DEMO-API-KEY"
     });
 
-    var requestOptions = {
+    let requestOptions = {
         method: 'GET',
         headers: headers,
         redirect: 'follow'
     };
+
+    let divUsers = document.querySelector('#div--users');
+
+    divUsers.innerHTML = `<div id="div--loading"><p>Loading ...</p><img id="img--dog" src="./assets/img/dog.gif" /></div>`;
 
     fetch("https://api.thedogapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=8", requestOptions)
         .then(response => response.json())
@@ -32,7 +35,7 @@ window.onload = _ => {
                 usersHTML += `<p class="p--description">${user.breeds[0].temperament}</p>`;
                 usersHTML += `</div>`;
             })
-            document.querySelector('#div--users').innerHTML = usersHTML;
+            divUsers.innerHTML = usersHTML;
         })
         .catch(error => console.log('error', error));
 }
